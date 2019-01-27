e5ce6691cf1e69e68afd753619d732b4f557fd3d
Grant Horner <h.grant.horner@gmail.com>
2019-01-27 15:06:48 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const morgan = require('morgan')
+const homePage = require('./views/homePage')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  const html = homePage()
+  res.send(html)
+})
+
+app.get('/users', (req, res) => {
+  const html = userPage()
+  res.send(html)
+})
+
+const PORT = 1337
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`)
+})
